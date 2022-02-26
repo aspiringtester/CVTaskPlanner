87fd9a1aae4537b3fd73d66e78ad45d09d27a7f1
paramita-Bhattacharya <paramitabhattacharya09@gmail.com>
2022-02-27 06:16:51 +1100

    committed

--- asset/js/index.js
+++ asset/js/index.js
@@ -1,118 +1,120 @@
 // Initialize a new TaskManager with currentId set to 0
 
-const taskManager = new TaskManager(0);
-console.log(taskManager);
+const taskManager = new TaskManager(0)
+console.log(taskManager)
 
-taskManager.load() // check localStorage 
-taskManager.render(); // render display
-
+taskManager.load() // check localStorage
+taskManager.render() // render display
+// accepting  only current and future dates in calendar
+let today = new Date().toISOString().split('T')[0]
+document.getElementById('dueDate').setAttribute('min', today)
 // SELECTOR FOR DATE AND TIME
-const dateElement = document.querySelector("#showDate");
-const timeElement = document.querySelector("#showTime");
+const dateElement = document.querySelector('#showDate')
+const timeElement = document.querySelector('#showTime')
 
 // FUNCTION FOR TIME OBJECT
 function tickingTime() {
-  let timeNow = new Date();
-  return (timeElement.innerText = `${timeNow.getHours()}:${timeNow.getMinutes()}:${timeNow.getSeconds()}`);
+  let timeNow = new Date()
+  return (timeElement.innerText = `${timeNow.getHours()}:${timeNow.getMinutes()}:${timeNow.getSeconds()}`)
 }
 // DATE OBJECT
-let d = new Date();
+let d = new Date()
 
 // DISPLAY TIME AND DATE
-tickingTime();
-setInterval(tickingTime, 1000);
-dateElement.innerText = `${d.getDate()} / ${d.getMonth() + 1} / ${d.getFullYear()}`;
-
+tickingTime()
+setInterval(tickingTime, 1000)
+dateElement.innerText = `${d.getDate()} / ${
+  d.getMonth() + 1
+} / ${d.getFullYear()}`
 
 // Close the modal after submit
 function closeModal() {
-  $('.modal').modal('hide');
+  $('.modal').modal('hide')
 }
 
 // CLEAR INPUTS AFTER SUBMIT
 const clearInputs = () => {
-  taskname.value = "";
-  description.value = "";
-  assignedTo.value = "";
-  getStatus.value = "Select";
-  dueDate.value = "";
-  NewErrorMessage.innerHTML = "";
-};
+  taskname.value = ''
+  description.value = ''
+  assignedTo.value = ''
+  getStatus.value = 'Select'
+  dueDate.value = ''
+  NewErrorMessage.innerHTML = ''
+}
 
 // NEW FORM SELECT VALUE
 const getSelectedValue = function () {
-  let val = document.querySelector("#getStatus").value;
-  return val;
-};
+  let val = document.querySelector('#getStatus').value
+  return val
+}
 
 // ERROR MESSAGE SELECTORS
-const NewErrorMessage = document.querySelector("#errorMessage");
-const EditErrorMessage = document.querySelector("#editErrorMessage");
+const NewErrorMessage = document.querySelector('#errorMessage')
+const EditErrorMessage = document.querySelector('#editErrorMessage')
 
-// FORM SELECTORS
-const newForm = document.querySelector("#newTask");
-const editForm = document.querySelector("#editTask");
+// FORM SELEcted
+const newForm = document.querySelector('#newTask')
 
 //ADD SUBMIT EVENT LISTENER
-newForm.addEventListener("submit", (error) => {
+newForm.addEventListener('submit', (error) => {
   // ALLOCATE SELECTORS
-  let taskname = document.querySelector("#taskname");
-  let description = document.querySelector("#description");
-  let assignedTo = document.querySelector("#assignedTo");
-  let dueDate = document.querySelector("#dueDate");
-  let taskNameError = document.querySelector(".taskNameError");
-  let descriptionError = document.querySelector(".descriptionError");
-  let assignedToError = document.querySelector(".assignedToError");
-  let dueDateError = document.querySelector(".dueDateError");
-  let statusError = document.querySelector(".statusError");
-  let newErrorMessage = [];
-  error.preventDefault();
+  let taskname = document.querySelector('#taskname')
+  let description = document.querySelector('#description')
+  let assignedTo = document.querySelector('#assignedTo')
+  let dueDate = document.querySelector('#dueDate')
+  let taskNameError = document.querySelector('.taskNameError')
+  let descriptionError = document.querySelector('.descriptionError')
+  let assignedToError = document.querySelector('.assignedToError')
+  let dueDateError = document.querySelector('.dueDateError')
+  let statusError = document.querySelector('.statusError')
+  let newErrorMessage = []
+  error.preventDefault()
 
-  console.log("Task Name :" + taskname.value.length);
-  console.log("Task Description :" + description.value.length);
-  console.log("Task Assigned To :" + assignedTo.value.length);
-  console.log("Task Due Date :" + dueDate.value);
-  console.log("Task Status:" + getSelectedValue());
+  console.log('Task Name :' + taskname.value.length)
+  console.log('Task Description :' + description.value.length)
+  console.log('Task Assigned To :' + assignedTo.value.length)
+  console.log('Task Due Date :' + dueDate.value)
+  console.log('Task Status:' + getSelectedValue())
 
   // VALIDATE NOT EMPTY STRING & HAS VALUE
-  if (taskname.value === "" || taskname.value == null) {
-    newErrorMessage.push("Enter a Task Name. ");
-    taskNameError.innerText = "Enter a Task Name. ";
+  if (taskname.value === '' || taskname.value == null) {
+    newErrorMessage.push('Enter a Task Name. ')
+    taskNameError.innerText = 'Enter a Task Name. '
   }
   // VALIDATE IF TASK NAME IS LONGER THAN 5 CHARACTERS
   if (taskname.value.length <= 5) {
-    newErrorMessage.push(`Name must be longer than 5 letters. `);
-    taskNameError.innerText = "Name must be longer than 5 letters.";
+    newErrorMessage.push(`Name must be longer than 5 letters. `)
+    taskNameError.innerText = 'Name must be longer than 5 letters.'
   }
 
   // VALIDATE IF DESCRIPTION IS LONGER THAN 5 CHARACTERS
   if (description.value.length <= 5) {
-    newErrorMessage.push(`Description must be longer than 5 letters. `);
-    descriptionError.innerText = `Description must be longer than 5 letters. `;
+    newErrorMessage.push(`Description must be longer than 5 letters. `)
+    descriptionError.innerText = `Description must be longer than 5 letters. `
   }
 
   // VALIDATE IF ASSIGNED TO NAME IS LONGER THAN 5 CHARACTERS
   if (assignedTo.value.length <= 5) {
-    newErrorMessage.push(`Assigned To must be longer than 5 letters. `);
-    assignedToError.innerText = `Assigned To must be longer than 5 letters. `;
+    newErrorMessage.push(`Assigned To must be longer than 5 letters. `)
+    assignedToError.innerText = `Assigned To must be longer than 5 letters. `
   }
 
   // VALIDATE IF DUE DATE HAS BEEN SELECTED
   if (dueDate.value.length < 1) {
-    newErrorMessage.push("Enter a valid date. ");
-    dueDateError.innerText = "Enter a valid date. ";
+    newErrorMessage.push('Enter a valid date. ')
+    dueDateError.innerText = 'Enter a valid date. '
   }
 
   // VALIDATE IF STATUS HAS A SELECTED VALUE
-  if (getSelectedValue() === "") {
-    newErrorMessage.push("Enter task status.");
-    statusError.innerText = 'Enter task status.';
+  if (getSelectedValue() === '') {
+    newErrorMessage.push('Enter task status.')
+    statusError.innerText = 'Enter task status.'
   }
   // VERIFY IF ARRAY CONTAINS ANY ERROR MESSAGES
   if (newErrorMessage.length > 0) {
-    error.preventDefault();
-    NewErrorMessage.innerHTML = newErrorMessage.join(" ");
-    NewErrorMessage.style.display = 'none';
+    error.preventDefault()
+    NewErrorMessage.innerHTML = newErrorMessage.join(' ')
+    NewErrorMessage.style.display = 'none'
   } else {
     // SUBMIT VALID FORM TO ARRAY
     taskManager.addTask(
@@ -120,23 +122,21 @@ newForm.addEventListener("submit", (error) => {
       description.value,
       assignedTo.value,
       dueDate.value,
-      getSelectedValue()
-    );
-    closeModal();
-    clearInputs();
-    taskNameError.innerText = "";
-    descriptionError.innerText = "";
-    assignedToError.innerText = "";
-    dueDateError.innerText = "";
-    statusError.innerText = "";
-    taskManager.render();
-    taskManager.save();
+      getSelectedValue(),
+    )
+    closeModal()
+    clearInputs()
+    taskNameError.innerText = ''
+    descriptionError.innerText = ''
+    assignedToError.innerText = ''
+    dueDateError.innerText = ''
+    statusError.innerText = ''
+    taskManager.render()
+    taskManager.save()
   }
-});
+})
 
-
-
-const taskList = document.querySelector("#taskList");
+const taskList = document.querySelector('#taskList')
 
 // ATTEMPT AT FUNCTION TO REMOVE VISIBILITY. ATTEMPTS AT STYLE.DISPLAY = 'NONE' WERE UNSUCCESSFUL
 /*function changeVisibility(element){
@@ -144,41 +144,36 @@ const taskList = document.querySelector("#taskList");
 }*/
 
 function showDiv() {
-  document.getElementById('markDone').style.display = "none";
+  document.getElementById('markDone').style.display = 'none'
 }
 
-
-
-taskList.addEventListener('click', (event)=> {
+taskList.addEventListener('click', (event) => {
   if (event.target.classList.contains('done-button')) {
-    const doneBTN = document.querySelector("#markDone");
-    const parentTask = event.target.parentElement.parentElement.parentElement;
+    const doneBTN = document.querySelector('#markDone')
+    const parentTask = event.target.parentElement.parentElement.parentElement
     console.log(parentTask)
-    const taskId = Number(parentTask.dataset.taskId);
-    const task = taskManager.getTaskById(taskId);
-    task.status = "Done";
+    const taskId = Number(parentTask.dataset.taskId)
+    const task = taskManager.getTaskById(taskId)
+    task.status = 'Done'
 
     //THE BELOW LINE OF CODE DOES NOT WORK BUT SHOULD MAKE THE MARK
     //DONE BUTTON DISSAPEAR
     //changeVisibility(doneBTN)
     taskManager.save()
-    taskManager.render();
+    taskManager.render()
 
-    showDiv();
+    showDiv()
   }
 
   if (event.target.classList.contains('delete-button')) {
-    const parentTask = event.target.parentElement.parentElement;
+    const parentTask = event.target.parentElement.parentElement
     console.log(parentTask)
-    const taskId = Number(parentTask.dataset.taskId);
-    taskManager.deleteTask(taskId);
-    taskManager.save();
-    taskManager.render();
+    const taskId = Number(parentTask.dataset.taskId)
+    taskManager.deleteTask(taskId)
+    taskManager.save()
+    taskManager.render()
   }
-
-});
-
-
+})
 
 // Still working on closing the add task modal on succesfull submission
 //of the form
--- asset/js/taskManager.js
+++ asset/js/taskManager.js
@@ -1,13 +1,12 @@
 const createTaskHtml = (id, name, description, assignedTo, dueDate, status) => {
-    let markDoneBtn = "";
+  let markDoneBtn = ''
 
-
-  if(status!== "done"){
-    markDoneBtn = `<button class="done-button" id="markDone" onclick="showDiv()">Mark as done</button>`;
+  if (status !== 'done') {
+    markDoneBtn = `<button class="done-button" id="markDone" onclick="showDiv()">MarkDone</button>`
   }
-  
-  const html = `<li class="list-group-item" data-task-id=${id}>
-  <div class="d-flex w-100 mt-2 justify-content-between align-items-center">
+
+  const html = `<li class="list-group-item taskDiv" data-task-id=${id}>
+  <div  class="d-flex w-100 mt-2 justify-content-between align-items-center ">
     <h5>Task Name : ${name}</h5>
     <span class="badge bg-danger text-light">${status}</span>
   </div>
@@ -20,19 +19,18 @@ const createTaskHtml = (id, name, description, assignedTo, dueDate, status) => {
   </div>
   <p>Task Description : ${description}</p>
   <section class="text-right">
-    <button type="button" class="btn btn-primary btn-sm" data-toggle="modal"
-      data-target="#editForm">EDIT</button>
+    
     <button class="delete-button btn btn-danger btn-sm" name="delete">DELETE</button>
   </section>
-</li>`;
-  return html;
-};
+</li>`
+  return html
+}
 
 // TASKMANAGER CLASS
 class TaskManager {
   constructor(currentId = 0) {
-    this.tasks = [];
-    this.currentId = currentId;
+    this.tasks = []
+    this.currentId = currentId
   }
 
   // ADD TASK METHOD
@@ -45,53 +43,53 @@ class TaskManager {
       description: description,
       assignedTo: assignedTo,
       dueDate: dueDate,
-      status: status
-    };
+      status: status,
+    }
 
-    this.tasks.push(task);
+    this.tasks.push(task)
   }
 
   // GET TASK BY ID METHOD
   getTaskById(taskId) {
-    let thisTask;
+    let thisTask
     // LOOP AND ALLOCATE CURRENT ID    (!! not currentId !!)
     for (let i = 0; i < this.tasks.length; i++) {
       // CURRENT TASK
-      const task = this.tasks[i];
+      const task = this.tasks[i]
       // CHECK IF ID IS SAME
       if (task.id === taskId) {
-        thisTask = task;
+        thisTask = task
       }
     }
     // Return the found task
-    return thisTask;
+    return thisTask
   }
 
   // DELETE TASK METHOD
   deleteTask(taskId) {
     //NEW TASK ARRAY WITHOUT DELETED TASK
-    const newTasks = [];
+    const newTasks = []
     // LOOP AND FIND CURRENT ID
-    for (let i=0; i<this.tasks.length; i++){
-      const task = this.tasks[i];
+    for (let i = 0; i < this.tasks.length; i++) {
+      const task = this.tasks[i]
       // AS PER INSTRUCTION
-      if (task.id !== taskId){
+      if (task.id !== taskId) {
         newTasks.push(task)
       }
     }
-    this.tasks = newTasks;
-  };
+    this.tasks = newTasks
+  }
 
   // RENDER METHOD
   render() {
-    let tasksHtmlList = [];
+    let tasksHtmlList = []
     // LOOP OVER TASKS ARRAY
     for (let i = 0; i < this.tasks.length; i++) {
-      const task = this.tasks[i];
+      const task = this.tasks[i]
       // CREATE NEW DATE
-      const date = new Date(task.dueDate);
+      const date = new Date(task.dueDate)
       const newDate =
-        date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear();
+        date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear()
       // CREATE VARIABLE FOR CREAT TASK FUNCTION
       const taskHtml = createTaskHtml(
         task.id,
@@ -99,38 +97,39 @@ class TaskManager {
         task.description,
         task.assignedTo,
         newDate,
-        task.status
-      );
+        task.status,
+      )
       // PUSH NEW HTML OF TASK TO ARRAY
-      tasksHtmlList.push(taskHtml);
+      tasksHtmlList.push(taskHtml)
     }
 
     // JOIN CONTENTS OF THE ARRAY
-    const tasksHtml = tasksHtmlList.join("\n");
+    const tasksHtml = tasksHtmlList.join('\n')
 
     // ALLOCATE SELLECTOR AND DISPLAY RESULTS OF ARRAY
-    const tasksList = document.querySelector("#taskList");
-    tasksList.innerHTML = tasksHtml;
+    const tasksList = document.querySelector('#taskList')
+    tasksList.innerHTML = tasksHtml
   }
 
   // save method
   save() {
-    let tasksJson = JSON.stringify(this.tasks); // create JSON string format
-    localStorage.setItem("tasks", tasksJson); // store under key name "tasks"
-    let currentId = JSON.stringify(this.currentId); // convert currentId into a JSON string
-    localStorage.setItem("currentId", currentId); // store under key 'CurrentId'
-
-  };
+    let tasksJson = JSON.stringify(this.tasks) // create JSON string format
+    localStorage.setItem('tasks', tasksJson) // store under key name "tasks"
+    let currentId = JSON.stringify(this.currentId) // convert currentId into a JSON string
+    localStorage.setItem('currentId', currentId) // store under key 'CurrentId'
+  }
 
   // load method
   load() {
-    if (localStorage.getItem("tasks")) {  // check the localStorage
-      let tasksJson = localStorage.getItem("tasks");
-      this.tasks = JSON.parse(tasksJson); // converts taskJson string to array
-    };
-    if (localStorage.getItem("currentId")) { // check localStorage for saved currentId
-      let currentId = localStorage.getItem("currentId");
-      this.currentId = parseInt(currentId); // convert currentId to a number
-    };
-  };
-};
+    if (localStorage.getItem('tasks')) {
+      // check the localStorage
+      let tasksJson = localStorage.getItem('tasks')
+      this.tasks = JSON.parse(tasksJson) // converts taskJson string to array
+    }
+    if (localStorage.getItem('currentId')) {
+      // check localStorage for saved currentId
+      let currentId = localStorage.getItem('currentId')
+      this.currentId = parseInt(currentId) // convert currentId to a number
+    }
+  }
+}
